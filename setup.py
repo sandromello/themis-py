ddeb7b8a9d029308ee141740550fd6513f05e7b1
Sandro Mello <sandro.mello@inova.net>
2015-01-01 16:40:36 -0300

    fix syntax error

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ packages = {
     'description' : 'Postfix milter behavior rate limiter',
     'long_description' : 'Themis is a policy daemon to predict and control the rate of sending mails in Postfix. \
       Is designed for large scale mail hosting environments, build on top of the python-milter API. \
-      The features was built not only for rate limiting but also to provide useful information about your mail environment.'
+      The features was built not only for rate limiting but also to provide useful information about your mail environment.',
     'data_files' : [
         ('/etc/themis', ['src/config/config.yaml']),
         ('/etc/init.d', ['src/init.d/themisd']),
