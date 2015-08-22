fb6db15be1832ef627840bff90a66b2ccda6a923
Sandro Mello <sandromll@gmail.com>
2015-08-22 14:04:21 -0300

    fixing modules

--- src/themis/components/__init__.py
+++ src/themis/components/__init__.py
@@ -1,1 +1,13 @@
-from .models import Policy, MetaData, Policy, ActionHeader, Group, Pool
+from .redismodels import (
+  Policy, MetaData, Policy, ActionHeader, 
+  Group, Pool, JAILBY_VALUES, JAILACTION_VALUES,
+  POLICY_TYPES, RESERVED_NAMES, Encoder
+)
+
+from .manager import Manager
+
+from .exceptions import (
+  ThemisException,
+  InconsistencyError,
+  ItemNotFound
+)
